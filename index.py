a9abea6af0b6f878453566082dec50308b30fa63
ioanaamaria1 <Pop.Mi.Ioana@student.utcluj.ro>
2024-10-05 18:15:17 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,14 @@
+print("hello")
+x="Ioana Maria Pop de la info anu 1"
+print(x)
+n=2
+if n % 2 == 0:
+    print("Acest numar este par")
+else:
+    print("Acest numar este impar")
+
+for x in range(10):
+    if x % 2 == 0:
+        print(str(x) + " este par")
+    else:
+        print(str(x)+" este impar")
